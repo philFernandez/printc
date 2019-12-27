2e15aaccdadafae6c0d92b4d9eb006d1bac6a025
Phil Fernandez <norcal618@gmail.com>
2019-12-26 16:15:45 -0800

    use this dir

--- printc.plugin.zsh
+++ printc.plugin.zsh
@@ -2,9 +2,10 @@
 # ~/.zcompdump-${SHORT_HOST}-${ZSH_VERSION} file
 autoload -Uz _printc && compinit -d $ZSH_COMPDUMP
 
+
 # Wrapper function to call included printc script
 function printc() {
-  zsh $ZSH/plugins/printc/printc_scpt $@
+  zsh $0:h/printc_scpt $@
 }
 
 
